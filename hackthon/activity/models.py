ddbb486e76ed7e55e5d0a07070eb9d605de6a398
Small-funny <876398710@qq.com>
2019-04-20 13:42:44 +0800

    feat: Models of database

--- hackthon/activity/models.py
+++ hackthon/activity/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class UserActivity(models.Model):
+    user_id = models.CharField(max_length=255)
+    activity_id = models.CharField(max_length=255)
+    user_class = models.CharField(max_length=2,choices=((u'S',u'Sponsor'),(u'A'),(u'Actor')))
--- hackthon/user/models.py
+++ hackthon/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.AutoField
+    nickName = models.CharField(max_length=30)
+    avatarUrl = models.CharField(max_length=100)
+    gender = models.CharField(max_length=2 , choices=((u'M',u'Male'),(u'F',u'Female')))
+    openId = models.CharField(max_length=30)
+
