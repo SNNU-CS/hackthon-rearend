2e25bece78b78eaf53baeec54c3148e21268f203
Qi Zhao <zhaoqi99@outlook.com>
2019-04-21 02:50:28 +0800

    feat: add ActivityUser get,post api

--- hackthon/activity/urls.py
+++ hackthon/activity/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 from .views import ActivityView, ActivityListView, UserActivityView
 urlpatterns = [
-    url(r'^user/(?P<id>[0-9]+)/', UserActivityView.as_view()),
+    url(r'^user/(?P<id>[\w\W]+)/', UserActivityView.as_view()),
     url(r'list/', ActivityListView.as_view()),
     url(r'^(?P<pk>[0-9]+)/', ActivityView.as_view()),
     url(r'', ActivityView.as_view()),
