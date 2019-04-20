ebcebfab28fa1279f6af65535c67a93f994dac3a
Qi Zhao <zhaoqi99@outlook.com>
2019-04-20 15:24:12 +0800

    feat(activity): add Model Activity

--- hackthon/activity/models.py
+++ hackthon/activity/models.py
@@ -6,3 +6,14 @@ class UserActivity(models.Model):
     user_id = models.IntegerField()
     activity_id = models.IntegerField()
     user_class = models.TextField(choices=((u'S', u'发起者'), (u'A', u'参与者')))
+
+
+class Activity(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    time = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    activity_type = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    delete_time = models.DateTimeField(default=None)
