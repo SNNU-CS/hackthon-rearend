c5d31185afdce5852d8ade8ef8276ad6edb89e80
Qi Zhao <zhaoqi99@outlook.com>
2019-04-20 22:25:18 +0800

    refactor(activity): add activity list api

--- hackthon/activity/urls.py
+++ hackthon/activity/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
-from .views import ActivityView
+from .views import ActivityView, ActivityListView
 urlpatterns = [
     # url(r'^user/(?P<pk>[0-9]+)/', UserActivityView.as_view()),
+    url(r'list/', ActivityListView.as_view()),
     url(r'^(?P<pk>[0-9]+)/', ActivityView.as_view()),
     url(r'', ActivityView.as_view()),
 ]
