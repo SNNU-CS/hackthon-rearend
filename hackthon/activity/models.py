bb8e18fc48047a1c2f124cdb3d934767e6bc791e
Qi Zhao <zhaoqi99@outlook.com>
2019-04-20 14:39:53 +0800

    style: format code using pep8

--- hackthon/activity/models.py
+++ hackthon/activity/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class UserActivity(models.Model):
     user_id = models.CharField(max_length=255)
     activity_id = models.CharField(max_length=255)
-    user_class = models.CharField(choices=((u'S',u'发起者'),(u'A',u'参与者')))
+    user_class = models.CharField(choices=((u'S', u'发起者'), (u'A', u'参与者')))
--- hackthon/user/models.py
+++ hackthon/user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     nick_name = models.CharField(max_length=30)
     avatar_url = models.CharField(max_length=100)
-    gender = models.CharField(choices=((u'M',u'男'),(u'F',u'女'),(u'U',u'未知')))
+    gender = models.CharField(choices=((u'M', u'男'), (u'F', u'女'), (u'U',
+                                                                    u'未知')))
     open_id = models.CharField(max_length=30)
-
