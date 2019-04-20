d9dbe311992bbe60743986da9f5d682a75eadb70
Small-funny <876398710@qq.com>
2019-04-20 13:59:57 +0800

    fixed: Models Name

--- hackthon/user/models.py
+++ hackthon/user/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.AutoField
-    nickName = models.CharField(max_length=30)
-    avatarUrl = models.CharField(max_length=100)
+    nick_name = models.CharField(max_length=30)
+    avatar_url = models.CharField(max_length=100)
     gender = models.CharField(max_length=2 , choices=((u'M',u'Male'),(u'F',u'Female')))
-    openId = models.CharField(max_length=30)
+    open_id = models.CharField(max_length=30)
 
