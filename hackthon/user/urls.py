c1c8a7869e40ddbee5b22f566823314d3bdd0eb3
Qi Zhao <zhaoqi99@outlook.com>
2019-04-21 02:39:09 +0800

    feat(user): add user api

--- hackthon/user/urls.py
+++ hackthon/user/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path, include
+from .views import UserView
 
-urlpatterns = []
+urlpatterns = [path('', UserView.as_view())]
