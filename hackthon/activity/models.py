3072cc6da3a94daacbcf94d09531b40fefa9a167
Qi Zhao <zhaoqi99@outlook.com>
2019-04-20 15:05:54 +0800

    fix(UserActivity): fix user_id,activty_id type

--- hackthon/activity/models.py
+++ hackthon/activity/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class UserActivity(models.Model):
-    user_id = models.CharField(max_length=255)
-    activity_id = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    activity_id = models.IntegerField()
     user_class = models.TextField(choices=((u'S', u'发起者'), (u'A', u'参与者')))
